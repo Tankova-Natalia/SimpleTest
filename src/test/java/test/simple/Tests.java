d15b690a0219c3664ab122c21f2bf8f52a4bdb2a
Natalia <tankova.natalya@bk.ru>
2023-06-06 16:03:06 +0300

    Добавила suite.

--- src/test/java/test/simple/Tests.java
+++ src/test/java/test/simple/Tests.java
@@ -1,11 +1,13 @@
 package test.simple;
 
+import io.qameta.allure.Feature;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import steps.Steps;
 
 public class Tests {
     @Test
+    @Feature("Проверка")
     public void test(){
         Steps.step(2);
     }
